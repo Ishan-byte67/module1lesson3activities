64c50f7cf9fec76ca4b3fd55ca93abaecd710f56
Ishan-byte67 <jaydeeph@gmail.com>
2026-05-25 21:01:41 +0100

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,22 @@
+name = "ishan"
+age = 11
+isstudent = True
+weight = 36.7
+
+print("name = ",name)
+print(type(name))
+
+print("age = ",age)
+print(type(age))
+
+print("isstudent = ",isstudent)
+print(type(isstudent))
+
+print("weight = ",weight)
+print(type(weight))
+
+age=str(age)
+print(type(age))
+
+weight=int(weight)
+print(type(weight))
